dc2bc49b1dada3817b3ceb9eb39a0110fd268d0c
clreaume <caralreaume@gmail.com>
2018-01-23 14:18:07 -0500

    initial commit

---
+++ src/NewGitDemo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your first name: ");
+		String name = input.nextLine();
+		System.out.println(name);
+
+	}
+
+}
